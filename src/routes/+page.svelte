7b396ee23640f0446fce051b698ede2a1615f32c
Marko Leinikka <mleinikka@gmail.com>
2024-02-28 17:51:34 +0900

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Lasikuu freelance software development and consulting." />
+</svelte:head>
+
+<section>
+	<h1>Welcome to Lasikuu</h1>
+	<h2>ラシクウへようこそ</h2>
+	<p>Freelance Software Development & Consulting</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About us</h1>
+
+	<p>
+		Offering freelance software development and consulting services in English, Japanese and Finnish.
+	</p>
+
+	<pre>building...</pre>
+
+</div>
