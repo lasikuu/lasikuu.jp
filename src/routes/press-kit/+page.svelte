16a0d5f39d74648207a4a51210b1119632df568d
Marko Leinikka <mleinikka@gmail.com>
2024-07-23 19:11:02 +0900

    fix: change the path for presskit files not to overlap with the page name

--- src/routes/press-kit/+page.svelte
+++ src/routes/press-kit/+page.svelte
@@ -15,15 +15,15 @@
 	<h2 class="grid">{$_('presskit.logo')}</h2>
 
 	<div class="mt-4 grid">
-		<img src="/press-kit/lasikuu-logo.svg" width="128" height="128" alt="Lasikuu logo" />
+		<img src="/presskit/lasikuu-logo.svg" width="128" height="128" alt="Lasikuu logo" />
 		<br />
-		<a href="/press-kit/lasikuu-logo.svg" download="lasikuu-logo.svg">{$_('presskit.download')} lasikuu-logo.svg</a>
-		<a href="/press-kit/lasikuu-logo.png" download="lasikuu-logo.png">{$_('presskit.download')} lasikuu-logo.png</a>
+		<a href="/presskit/lasikuu-logo.svg" download="lasikuu-logo.svg">{$_('presskit.download')} lasikuu-logo.svg</a>
+		<a href="/presskit/lasikuu-logo.png" download="lasikuu-logo.png">{$_('presskit.download')} lasikuu-logo.png</a>
 	</div>
 
 	<h2 class="mt-16 grid">{$_('presskit.meishi')}</h2>
 
-	<a href="/press-kit/meishi_lasikuu_marko.pdf" target="_blank" rel="noopener noreferrer">
+	<a href="/presskit/meishi_lasikuu_marko.pdf" target="_blank" rel="noopener noreferrer">
 		{$_('presskit.download')} meishi_lasikuu_marko.pdf
 	</a>
 	<br />
