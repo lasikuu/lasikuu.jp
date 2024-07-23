08f48c11eefd3fc30baf3e88e0b57bf2f6254575
Marko Leinikka <mleinikka@gmail.com>
2024-07-23 19:10:11 +0900

    feat: add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<div class="block text-center">
+	<h3>
+		{#if $page.status === 404}
+			404 - not found
+		{:else if $page.status === 403}
+			403 - forbidden
+		{:else}
+			500 - server error
+		{/if}
+	</h3>
+</div>
